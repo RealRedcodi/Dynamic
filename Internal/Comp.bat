cf550e1a8fc85064d19b9e6c1a591aae4488452b
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:28:46 -0500

    Why did I move this

--- Internal/Comp.bat
+++ Internal/Comp.bat
@@ -1,4 +1,4 @@
-7zr.exe a dynamic.7z Dynamic\Dynamic.bat
+7zr.exe a dynamic.7z Dynamic.bat
 copy /b dynamic.sfx + config.txt + dynamic.7z ..\Builds\Dynamic.exe
 upx --brute ..\Builds\Dynamic.exe
 del "dynamic.7z"
