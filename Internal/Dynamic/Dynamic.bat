cbb96d1442e8be798d195aeaf7d9772c2bdf743e
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:26:05 -0500

    Update Dynamic.bat

--- Internal/Dynamic/Dynamic.bat
+++ Internal/Dynamic/Dynamic.bat
@@ -1,6 +1,7 @@
 @echo off
 cls
-title Dynamic
-echo Dynamic
-echo Soon:tm:
+title Dynamic Downloader From Redcodi Games
+echo Dynamic Downloader From Redcodi Games
+Invoke-WebRequest -Uri "https://github.com/RealRedcodi/Dynamic/raw/b72641b95b7945690d19cd5ac888433e61768fb4/TESTDL.txt" -OutFile "TESTDL.txt"
+TESTDL.txt
 pause
