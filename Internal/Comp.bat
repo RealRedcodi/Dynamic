e119310e8f6918eae343ada28f202e83cdc86873
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:06:00 -0500

    Change build location

--- Internal/Comp.bat
+++ Internal/Comp.bat
@@ -1,5 +1,3 @@
 7zr.exe a dynamic.7z Dynamic.bat
-copy /b dynamic.sfx + config.txt + dynamic.7z ..\Dynamic.exe
-pause
+copy /b dynamic.sfx + config.txt + dynamic.7z ..\Builds\Dynamic.exe
 del "dynamic.7z"
-pause
