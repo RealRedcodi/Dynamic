b904cacbac065d49ff4092fcaff32ab5019b206c
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:03:27 -0500

    Move to new location

--- Internal/Comp.bat
+++ Internal/Comp.bat
@@ -1,5 +1,5 @@
 7zr.exe a dynamic.7z Dynamic.bat
-copy /b dynamic.sfx + config.txt + dynamic.7z Dynamic.exe
+copy /b dynamic.sfx + config.txt + dynamic.7z ..\Dynamic.exe
 pause
 del "dynamic.7z"
 pause
