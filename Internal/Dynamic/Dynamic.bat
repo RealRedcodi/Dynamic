2d4be2938327339eb3ab7afef3aac2c5ab4c88ee
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:26:37 -0500

    forgot powershell

--- Internal/Dynamic/Dynamic.bat
+++ Internal/Dynamic/Dynamic.bat
@@ -2,6 +2,6 @@
 cls
 title Dynamic Downloader From Redcodi Games
 echo Dynamic Downloader From Redcodi Games
-Invoke-WebRequest -Uri "https://github.com/RealRedcodi/Dynamic/raw/b72641b95b7945690d19cd5ac888433e61768fb4/TESTDL.txt" -OutFile "TESTDL.txt"
+powershell Invoke-WebRequest -Uri "https://github.com/RealRedcodi/Dynamic/raw/b72641b95b7945690d19cd5ac888433e61768fb4/TESTDL.txt" -OutFile "TESTDL.txt"
 TESTDL.txt
 pause
