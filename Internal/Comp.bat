23a258adb76bb0702f03a75212930e1b61da468a
RealRedcodi <killianr1@gmail.com>
2021-11-17 17:18:08 -0500

    Trying to setup

--- Internal/Comp.bat
+++ Internal/Comp.bat
@@ -1,5 +1,4 @@
-7zr.exe a dynamic.7z Dynamic.bat
+7zr.exe a dynamic.7z Dynamic\Dynamic.bat
 copy /b dynamic.sfx + config.txt + dynamic.7z ..\Builds\Dynamic.exe
 upx --brute ..\Builds\Dynamic.exe
 del "dynamic.7z"
-pause
